babaf53c06358095f2f19ed8993d1251607fb30c
ntpadovani <normant_padovani@hotmail.com>
2023-04-10 20:19:44 -0400

    task 3

---
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Using what you did in the task #0, extend your
+Python script to export ALL data in the JSON format.
+API used: https://jsonplaceholder.typicode.com/
+"""
+
+
+if __name__ == "__main__":
+    import json
+    import requests
+
+    # Endpoint URL
+    users_response = requests.get("https://jsonplaceholder.typicode.com/users")
+    users = users_response.json()
+
+    users_todo_dict = {}
+
+    for user in users:
+        # Extracting actual user id for extracts its TODO tasks
+        user_id = user.get('id')
+        user_todo_query = {'userId': user_id}
+        response_2 = requests.get("https://jsonplaceholder.typicode.com/todos",
+                                  params=user_todo_query)
+        todo_list = response_2.json()
+
+        # Creating a list of user's tasks (each contained in dictionary form)
+        username = user.get('username')
+        tasks = [{"task": task.get('title'), "username": username,
+                  "completed": task.get('completed')} for task in todo_list]
+
+        # Updating users task's dictionary with actual user and its tasks
+        users_todo_dict[user_id] = tasks
+
+    # Serializing to json
+    json_object = json.dumps(users_todo_dict)
+
+    # Writing to json file
+    with open('todo_all_employees.json', "w") as jsonfile:
+        jsonfile.write(json_object)
